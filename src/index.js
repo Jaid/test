a3cf7eaf4ec79d5aa9c5192ad011e8989071017d
Jaid <jaid.jsx@gmail.com>
2019-12-01 16:37:01 +0100

    Added JSDoc comment
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 /** @module test */
 
-export default () => 1 + 1
+/**
+ * Adds second argument to first argument
+ * @param {number} firstNumber First number
+ * @param {number} [secondNumber=1] Second number
+ * @return {number} The sum of both arguments
+ * @example
+ * import test from "test"
+ * const firstNumber = 5
+ * const secondNumber = 7
+ * const result = test(firstNumber, secondNumber)
+ * result === 12
+ * @example
+ * import test from "test"
+ * const number = 5
+ * const result = test(number)
+ * result === 6
+ */
+export default (firstNumber, secondNumber = 1) => firstNumber + secondNumber
